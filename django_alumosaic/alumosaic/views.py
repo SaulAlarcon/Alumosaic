1b68a5b36bf5d14baa2a4efea9cdc9ec2c5592ed
Saul Alarcon <alarcon.saul@gmail.com>
2021-12-31 16:33:23 -0600

    Adding basic django html pages

--- django_alumosaic/alumosaic/views.py
+++ django_alumosaic/alumosaic/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+gallery = [
+    {
+        'author': 'Saul',
+        'title': 'Test Post 1',
+        'content': 'Test Content 1',
+        'date_posted': 'December 26, 2021'
+    },
+    {
+        'author': 'Saul',
+        'title': 'Test Post 2',
+        'content': 'Test Content 2',
+        'date_posted': 'December 26, 2021'
+    }
+]
+
+def home(request):
+    context = {
+        'gallery': gallery
+    }
+    return render(request, 'alumosaic/home.html', context)
+
+def about(request):
+    return render(request, 'alumosaic/about.html', {'title': 'About'})
+
